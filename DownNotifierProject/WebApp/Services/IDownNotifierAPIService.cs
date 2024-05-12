a6e182e03cf8e00e8b2cee305323ea7d99e339cf
sdeveci <kyros06@hotmail.com>
2024-05-13 01:33:37 +0300

    TargetApp CRUD operations were created. And refit, the httpclient nuget package, was added to the project. (#8)

---
+++ DownNotifierProject/WebApp/Services/IDownNotifierAPIService.cs
@@ -0,0 +1,19 @@
+﻿using DownNotifier.API.Entities;
+using Refit;
+namespace DownNotifier.WebApp.Services
+{
+    public interface IDownNotifierAPIService
+    {
+        [Get("/api/TargetApp")]
+        Task<IEnumerable<TargetApp>> GetAll();
+
+        [Post("/api/TargetApp")]
+        Task Insert(TargetApp pReq);
+
+        [Put("/api/TargetApp/{id}")]
+        Task Update(int id, TargetApp pReq);
+
+        [Delete("/api/TargetApp/{id}")]
+        Task Delete(int id);
+    }
+}
