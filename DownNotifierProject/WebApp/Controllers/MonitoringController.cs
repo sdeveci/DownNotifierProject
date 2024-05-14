7ff018e88fda1982718f40507add6ea7a6b749ce
sdeveci <kyros06@hotmail.com>
2024-05-14 19:28:21 +0300

    Monitoring review operations were done. (#21)

--- DownNotifierProject/WebApp/Controllers/MonitoringController.cs
+++ DownNotifierProject/WebApp/Controllers/MonitoringController.cs
@@ -1,4 +1,5 @@
-﻿using DownNotifier.API.Services;
+﻿using DownNotifier.API.Entities;
+using DownNotifier.API.Enums;
 using DownNotifier.WebApp.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,19 +8,24 @@ namespace DownNotifier.WebApp.Controllers
     public class MonitoringController : Controller
     {
         private readonly DownNotifierAPIService _apiService;
-
-        public MonitoringController(DownNotifierAPIService apiService)
+        private readonly UserSessionService _userSessionService;
+        private readonly int _userId;
+        public MonitoringController(DownNotifierAPIService apiService ,UserSessionService userSessionService)
         {
             _apiService = apiService;
+            _userSessionService = userSessionService;
+            _userId = int.Parse(_userSessionService.GetUserId());
         }
         public async Task<IActionResult> Index()
         {
             return View();
         }
 
-        public async Task<IActionResult> HealthCheck()
-        {   
-          await _apiService.Api.HealthCheck();
+        public async Task<IActionResult> HealthCheck(TargetApp pReq)
+        {
+                pReq.AplicationUserId = _userId;
+                pReq.NotificationType = NotificationType.Email;
+                await _apiService.Api.HealthCheck(pReq);
           return View();
         }
     }
